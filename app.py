9d41b84040db0cf81e8959c43d94f2b0224dc2ae
louieeeee <louiesanqui0518@gmail.com>
2024-08-03 14:36:45 +0800

    Added Procfile

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+import os
+
 from flask import Flask, render_template
 from views import app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)), debug=False)
+
